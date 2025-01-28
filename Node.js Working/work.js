2f3e13b26f0eea09fa98e950ec05681a64e51b40
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2025-01-28 17:56:28 +0530

    Add files via upload

---
+++ Node.js Working/work.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+
+//   Give 'os' information
+
+const os  = require('os');   
+console.log(os.cpus().length);
+
+// Blocking the Request
+
+console.log("visual studio");   //  >>  Testing Command Step 
+
+const result = fs.readFileSync("symbol.txt", "utf-8");
+console.log(result);
+
+console.log("tools and tech");   //  >>  Testing Command Step 
+
+
+
+// Non - Blocking the Request
+
+fs.readFileSync("symbol.txt", "utf-8", (err, result1 ) => {
+    console.log(result1);
+});
+
+console.log("node.js server");   //  >>  Testing Command Step 
+
