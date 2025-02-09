5f20e3f317a3e9ed002af64f36c5ff035de651ea
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2025-02-09 16:10:26 +0530

    Update http.js

--- server/http.js
+++ server/http.js
@@ -1,4 +1,101 @@
 /*
+
+    Get : 
+    >> When we want to get some data from the server
+
+    Post :
+    >> When we want to send some data to the server for creating or updating a resource
+
+    Put :
+    >> When we want to send some data to the server to update an existing resource
+
+    Patch :
+    >> When we want to send some data to the server to partially update an existing resource
+
+    Delete :
+    >> When we want to delete some data from the server for a resource
+
+    Head :
+    >> When we want to get the HTTP headers of a resource without the body content
+
+    Options :
+    >> When we want to get the HTTP methods supported by a resource
+
+    Trace :
+    >> When we want to trace the request-response cycle to the server
+
+    Connect :
+    >> When we want to establish a connection to a server
+
+    Link :
+    >> When we want to link related resources
+
+    Unlink :
+    >> When we want to unlink related resources
+
+    Purge :
+    >> When we want to purge a resource
+
+    Check :
+    >> When we want to check the status of a resource
+
+    Search :
+    >> When we want to search for a resource
+
+    Merge :
+    >> When we want to merge resources
+
+    Mkactivity :
+    >> When we want to create an activity
+
+    Mkcol :
+    >> When we want to create a new collection
+
+    Move :
+    >> When we want to move a resource
+
+    Copy :
+    >> When we want to copy a resource
+
+    Lock :
+    >> When we want to lock a resource
+
+    Unlock :
+    >> When we want to unlock a resource
+
+    Subordinate :
+    >> When we want to create a subordinate resource
+
+    Propertyfind :
+    >> When we want to retrieve properties of a resource
+
+    Propertyupdate :
+    >> When we want to update properties of a resource
+
+    Rebind :
+    >> When we want to rebind a resource
+
+    Notify :
+    >> When we want to notify a resource
+
+    Subscribe :
+    >> When we want to subscribe to a resource
+
+    Unsubscribe :
+    >> When we want to unsubscribe from a resource
+
+    Searchres :
+    >> When we want to search for a resource
+
+    Autoreplay :
+    >> When we want to automatically reply to a resource
+
+    Mergepatch :
+    >> When we want to merge and patch resources
+
+    Mergeforcepatch :
+    >> When we want to merge and force-patch resources
+*//*
     Get
     Post
     Put
