4dcf616bec15a2044b2f4878498f91df411ff795
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2025-02-11 17:57:28 +0530

    Add files via upload

---
+++ express/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello From Home Page");
+});
+
+app.get("/about", (req, res) =>{
+    return res.send("Hello From About Page");
+});
+
+
+app.get("/main", (req, res) =>{
+    return res.send(`Hello ${req.query.name}`); //http://localhost:8011/main?name=anmol
+});
+
+
+/*
+app.get("/main", (req, res) => {
+    console.log(req.query); 
+    const name = req.query.name || "Guest";
+    const age = req.query.age || "Unknown";
+    return res.send(`Hello ${name} or Your Age is ${age}`);
+});
+*/
+
+app.listen(8011, () => console.log("Server running on port 8011")); 
